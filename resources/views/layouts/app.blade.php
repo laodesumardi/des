c2e829e71ab738c520599ea0578c29dc475a83e4
laodesumardi <sumardilaode04@gmail.com>
2026-01-22 22:42:20 +0900

    Feat: Perbesar ukuran logo di header dan footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,9 +93,9 @@
                 <!-- Logo -->
                 <div class="logo-animate px-4 py-3 md:px-5 md:py-4 flex items-center justify-center overflow-hidden">
                     @if($logoPath)
-                    <img src="{{ asset('images/' . $logoPath) }}?v={{ time() }}" alt="{{ $headerNamaDesa }}" class="w-12 h-12 md:w-16 md:h-16 object-contain">
+                    <img src="{{ asset('images/' . $logoPath) }}?v={{ time() }}" alt="{{ $headerNamaDesa }}" class="w-16 h-16 md:w-20 md:h-20 object-contain">
                     @else
-                    <svg class="w-12 h-12 md:w-16 md:h-16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <svg class="w-16 h-16 md:w-20 md:h-20" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
                     </svg>
                     @endif
@@ -210,9 +210,9 @@
                     <div class="flex items-center gap-3 mb-4">
                         <div class="p-2 overflow-hidden">
                             @if($logoPath)
-                            <img src="{{ asset('images/' . $logoPath) }}?v={{ time() }}" alt="{{ $headerNamaDesa }}" class="w-6 h-6 object-contain">
+                            <img src="{{ asset('images/' . $logoPath) }}?v={{ time() }}" alt="{{ $headerNamaDesa }}" class="w-8 h-8 object-contain">
                             @else
-                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
                             </svg>
                             @endif
